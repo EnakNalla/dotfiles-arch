81b40bd6c3cdcc6e6edf15b37d08e211bb34d2e5
EnakNalla <121622489+EnakNalla@users.noreply.github.com>
2024-10-02 21:38:17 +0100

    include stow

--- scripts/arch-postinstall.sh
+++ scripts/arch-postinstall.sh
@@ -1,6 +1,10 @@
 # install yay
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
+sudo pacman -S --needed stow
+
+stow ..
+
 PACMAN_PACKAGES=(
 # desktop
   "hyprland"
